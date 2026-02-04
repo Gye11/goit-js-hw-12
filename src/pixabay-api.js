0ffd0e05ba31028b41287e73ddec80fb6e11d760
Gye11 <hilallornekk@gmail.com>
2026-02-04 03:32:33 +0300

    final

---
+++ src/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '54460001-4340b48267dd4c07393208eaa';
+
+export async function fetchImages(query, page) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page,
+      per_page: 20,
+    },
+  });
+
+  return response.data;
+}
