86c8e8a5eb89e99454d96e4c4e4959ee7265fa67
Gye11 <hilallornekk@gmail.com>
2026-02-06 03:05:05 +0300

    final

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '54460001-4340b48267dd4c07393208eaa';
 
-export async function fetchImages(query, page) {
+export async function fetchImages(query, page = 1) {
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
@@ -12,7 +12,7 @@ export async function fetchImages(query, page) {
       orientation: 'horizontal',
       safesearch: true,
       page,
-      per_page: 20,
+      per_page: 40,
     },
   });
 
