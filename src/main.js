9159ea7eaa4dcf2ebe853125268ad41f6f5e4ee9
Gye11 <hilallornekk@gmail.com>
2026-02-06 03:24:57 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import { fetchImages } from './pixabay-api.js';
 
-const form = document.querySelector('#search-form');
+const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const loadingText = document.querySelector('.loading-text');
 
 let page = 1;
 let query = '';
-let totalPages = 0;
 
 form.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSearch(e) {
   e.preventDefault();
-  query = e.target.searchQuery.value.trim();
+
+  query = e.target.elements[0].value.trim();
   page = 1;
   gallery.innerHTML = '';
   loadMoreBtn.classList.add('is-hidden');
@@ -24,32 +24,20 @@ async function onSearch(e) {
   loadingText.classList.remove('is-hidden');
 
   const data = await fetchImages(query, page);
-  totalPages = Math.ceil(data.totalHits / 20);
-
-  renderImages(data.hits);
 
   loadingText.classList.add('is-hidden');
 
-  if (page < totalPages) {
+  renderImages(data.hits);
+
+  if (data.totalHits > 40) {
     loadMoreBtn.classList.remove('is-hidden');
   }
 }
 
 async function onLoadMore() {
   page += 1;
-  loadingText.classList.remove('is-hidden');
-
   const data = await fetchImages(query, page);
   renderImages(data.hits);
-
-  loadingText.classList.add('is-hidden');
-
-  smoothScroll();
-
-  if (page >= totalPages) {
-    loadMoreBtn.classList.add('is-hidden');
-    alert("We're sorry, but you've reached the end of search results");
-  }
 }
 
 function renderImages(images) {
@@ -64,18 +52,9 @@ function renderImages(images) {
           <p>Comments ${img.comments}</p>
           <p>Downloads ${img.downloads}</p>
         </div>
-      </div>
-    `
+      </div>`
     )
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
-}
-
-function smoothScroll() {
-  const cardHeight = gallery.firstElementChild.getBoundingClientRect().height;
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
 }
