90b5df79f81adf9f996c38fb8d765dd2e9ef7787
Gye11 <hilallornekk@gmail.com>
2026-02-07 02:26:43 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { fetchImages } from './pixabay-api.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
@@ -7,52 +11,94 @@ const loadingText = document.querySelector('.loading-text');
 
 let page = 1;
 let query = '';
+let lightbox = new SimpleLightbox('.gallery a');
 
 form.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
 
-async function onSearch(e) {
+function onSearch(e) {
   e.preventDefault();
 
   query = e.target.elements[0].value.trim();
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
   page = 1;
   gallery.innerHTML = '';
   loadMoreBtn.classList.add('is-hidden');
-
-  if (!query) return;
-
   loadingText.classList.remove('is-hidden');
 
-  const data = await fetchImages(query, page);
+  fetchImages(query, page)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.info({
+          message: 'Sorry, no images found.',
+          position: 'topRight',
+        });
+        return;
+      }
 
-  loadingText.classList.add('is-hidden');
+      renderImages(data.hits);
+      lightbox.refresh();
 
-  renderImages(data.hits);
-
-  if (data.totalHits > 40) {
-    loadMoreBtn.classList.remove('is-hidden');
-  }
+      if (data.totalHits > 20) {
+        loadMoreBtn.classList.remove('is-hidden');
+      }
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      loadingText.classList.add('is-hidden');
+    });
 }
 
-async function onLoadMore() {
+function onLoadMore() {
   page += 1;
-  const data = await fetchImages(query, page);
-  renderImages(data.hits);
+
+  fetchImages(query, page)
+    .then(data => {
+      renderImages(data.hits);
+      lightbox.refresh();
+
+      if (gallery.children.length >= data.totalHits) {
+        loadMoreBtn.classList.add('is-hidden');
+        iziToast.info({
+          message: "You've reached the end of search results.",
+          position: 'topRight',
+        });
+      }
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Error loading more images.',
+        position: 'topRight',
+      });
+    });
 }
 
 function renderImages(images) {
   const markup = images
     .map(
       img => `
-      <div class="photo-card">
+      <a href="${img.largeImageURL}" class="photo-card">
         <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
         <div class="info">
-          <p>Likes ${img.likes}</p>
-          <p>Views ${img.views}</p>
-          <p>Comments ${img.comments}</p>
-          <p>Downloads ${img.downloads}</p>
+          <p><b>Likes</b> ${img.likes}</p>
+          <p><b>Views</b> ${img.views}</p>
+          <p><b>Comments</b> ${img.comments}</p>
+          <p><b>Downloads</b> ${img.downloads}</p>
         </div>
-      </div>`
+      </a>
+    `
     )
     .join('');
 
